3c9beff48e1bdaac781895bb4fab9617f5f09ce3
Yulia Demakova <j.demakova@gmail.com>
2021-12-10 15:47:20 +0100

    update server

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+const express = require('express')
+require('dotenv/config')
+require('./db')
 
-// ℹ️ Connects to the database
-require("./db");
+const app = express()
+require('./config')(app)
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require("express");
+const usersRouter = require('./ressources/user/user.routes')
+const articleRouter = require('./ressources/article/article.routes')
+app.use('/api/users', usersRouter)
+app.use('/api/articles', articleRouter)
+require('./error-handling')(app)
 
-const app = express();
-const usersRouter = require("./ressources/user/user.routes");
-const articleRouter = require("./ressources/article/article.routes");
-
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
-
-// 👇 Start handling routes here
-// Contrary to the views version, all routes are controlled from the routes/index.js
-const allRoutes = require("./routes");
-app.use("/api", allRoutes);
-
-app.use("/api/users", usersRouter);
-app.use("/api/articles", articleRouter);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
-
-module.exports = app;
+module.exports = app
